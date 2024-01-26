1137b39f9ccf63d782ae2733500368e40c105698
Daniel <williamsoligodaniel@gmail.com>
2024-01-26 13:44:11 +0100

    setting up vite and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a Tailwind CSS styled component.
+    </div>
+  );
+}
+
+export default MyComponent;
