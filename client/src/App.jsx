9569d7aadc8b447e12a197a212815b291d3953a8
Daniel <williamsoligodaniel@gmail.com>
2024-01-27 10:35:39 +0100

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
+// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import Reserve from './pages/Reserve';
 import About from './pages/About';
@@ -11,10 +13,38 @@ function App() {
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/reserve" element={<Reserve />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience-o2" element={<ExperienceO2 />} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/reserve"
+          element={
+            <Layout>
+              <Reserve />
+            </Layout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <About />
+            </Layout>
+          }
+        />
+        <Route
+          path="/experience-o2"
+          element={
+            <Layout>
+              <ExperienceO2 />
+            </Layout>
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// Header.jsx
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src="/alcasa-logo.jpg" alt="Logo" />
+    </header>
+  );
+}
+
+export default Header;
